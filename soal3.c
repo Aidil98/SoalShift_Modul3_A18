c859c5c18fe43861f60451dfb93ce924d4d88f44
Aidil98 <aidilabdillah98@gmail.com>
2017-10-25 10:46:16 +0700

    soal 3

--- soal3.c
+++ soal3.c
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-pthread_t thread_makan, thread_selesai;
+pthread_t thread_display, thread_makan, thread_selesai;
 int l = 100;
 int k = 100;    
 int i=1;
 int a;
+void *display () {
+    printf("Lohan                   Kepiting\n");
+    printf(" %d                     %d\n",l, k);
+    printf("menu :\n1 kasih makan Lohan\n2 Kasih makan Kepiting\n");
+}
 void *makan_bang() {
     printf ("beri makan :\n");
     scanf ("%d", &a);
@@ -15,11 +20,7 @@ void *makan_bang() {
     else if (a == 2)k=k+10;    
     else printf ("Makan bang\n");
 }
-/*void *game_over () {
-    if (l <= 0 || l > 100 || k <= 0 || k > 100) {
-    printf ("Game Over\n");
-    }
-}*/
+
 int main()
 {
     
@@ -29,16 +30,13 @@ int main()
         
         printf("%d\n",i); //supaya tahu waktunya
         fflush(stdout);
-        printf("Lohan                   Kepiting\n");
-        printf(" %d                     %d\n",l, k);
-        printf("menu :\n1 kasih makan Lohan\n2 Kasih makan Kepiting\n");
         sleep(1);
         system("clear");
     	i++;
     	if(i%10==0) l=l-15;
     	if(i%20==0) k = k-10;
+        pthread_create(&(thread_display), NULL, &display, NULL);
         pthread_create(&(thread_makan), NULL, &makan_bang, NULL);
- //       pthread_create(&(thread_selesai), NULL, &game_over, NULL);
         if (l <= 0 || l > 100 || k <= 0 || k > 100) {
             printf ("Game Over\n");
             break;
