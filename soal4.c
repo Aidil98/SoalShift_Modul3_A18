e5a3d892485be43597adedfe900c98f70a281967
Aidil98 <aidilabdillah98@gmail.com>
2017-10-25 17:34:08 +0700

    soal 4

--- soal4.c
+++ soal4.c
@@ -4,7 +4,6 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<stdlib.h>
-//#include<conio.h>
 /*******************************************************
 *compile dengan cara gcc -pthread -o [output] input.c *
 *******************************************************/
